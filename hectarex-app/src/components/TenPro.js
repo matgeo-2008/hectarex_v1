9419220c111bab8a06c77bef18114aa3b4bf7716
Mathew George <mac@Mathews-MacBook-Air.local>
2018-05-21 22:42:03 +0530

    Loading text added

--- hectarex-app/src/components/TenPro.js
+++ hectarex-app/src/components/TenPro.js
@@ -6,24 +6,51 @@ class TenPro extends Component {
     constructor() {
         super()
         this.state = {
+            fetchInProgress: true,
             tenant: [],
+        }
+        this.displayContent = this.displayContent.bind(this)
+    }
+
+    displayContent() {
+        if(this.state.fetchInProgress){
+            return (
+                <div>
+                    <h4>Loading...</h4>
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <h3>Tenant: {this.state.tenant.name}</h3>
+                    <br />Tenant ID: {this.state.tenant.tenantId}
+                </div>
+            )
         }
     }
 
     componentDidMount() {
         axios.get("http://localhost:3000/api/Tenant/TEN1")
         .then(response => {
-            this.setState({tenant: response.data})
+            this.setState({
+                fetchInProgress: false,
+                tenant: response.data
+            })
             console.log(this.state.tenant)
         })
-        .catch(err => {console.log(err)})
+        .catch(err => {
+            console.log(err)
+            this.setState({
+                fetchInProgress: false
+            })
+        })
     }
 
     render() {
         return(
             <div>
-                <h3>Tenant: {this.state.tenant.name}</h3>
-                <br />Tenant ID: {this.state.tenant.tenantId}
+                <h2>Tenant Profile Page</h2>
+                {this.displayContent()}
                 <br /><br />
                 <Link to="/">Home</Link>    
             </div>
